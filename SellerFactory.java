33af12721974f235223ce2b59dda6b86d866a50f
Sagi Belmash <121439223+SagiBelmash314@users.noreply.github.com>
2025-01-14 14:08:23 +0200

    Update SellerFactory.java

--- SellerFactory.java
+++ SellerFactory.java
@@ -1,6 +1,5 @@
 public class SellerFactory {
     public static Seller createSeller(String name, String password) {
-        return new Seller(name,password);
+        return new Seller(name, password);
     }
-
 }
